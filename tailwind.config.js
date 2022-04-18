a353dc1dd8bbf779146faa664d1b582b4e80a5a8
Henrik Fricke <henrikfricke@web.de>
2022-04-18 22:38:34 +0200

    feat: fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Jost', ...defaultTheme.fontFamily.sans],
+        'francois-one': ['Francois One', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
 }
