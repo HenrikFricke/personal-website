73a71bb9b1ebed49094ae2c750e6c33669709fdf
Henrik Fricke <henrikfricke@web.de>
2022-04-28 00:21:11 +0200

    feat(root): preload font and wave svg

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,27 @@ import {
 import styles from "./styles/app.css";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: styles },
+    {
+      rel: "preload",
+      href: "/fonts/jost/jost-v12-latin-regular.woff2",
+      as: "font",
+      type: "font/woff2",
+    },
+    {
+      rel: "preload",
+      href: "/fonts/francois-one/francois-one-v19-latin-regular.woff2",
+      as: "font",
+      type: "font/woff2",
+    },
+    {
+      rel: "preload",
+      href: "/img/wave.svg",
+      as: "image",
+      type: "image/svg+xml",
+    },
+  ];
 };
 
 export const meta: MetaFunction = () => ({
