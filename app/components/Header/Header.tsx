4c9fb0f9a105af85536a97d9b13a1e33c531130d
Henrik Fricke <henrikfricke@web.de>
2022-04-27 18:43:38 +0200

    refactor(Header): finetuning margin/padding

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -11,8 +11,8 @@ export interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = (props) => {
   return (
-    <header className="bg-primary p-6 lg:pt-20 lg:pb-20 min-h-screen flex items-center justify-center">
-      <section className="z-10 md:mx-16 relative max-w-4xl before:absolute before:bg-sedondary before:inset-0 before:translate-x-1 before:translate-y-1 lg:before:translate-x-2 lg:before:translate-y-2 after:absolute after:bg-tertiary after:inset-0 after:translate-x-2 after:translate-y-2 lg:after:translate-x-4 lg:after:translate-y-4 after:-z-10">
+    <header className="bg-primary p-6 md:p-16 min-h-screen flex items-center justify-center">
+      <section className="z-10 mr-2 mb-2 lg:mr-4 lg:mb-4 relative max-w-4xl before:absolute before:bg-sedondary before:inset-0 before:translate-x-1 before:translate-y-1 lg:before:translate-x-2 lg:before:translate-y-2 after:absolute after:bg-tertiary after:inset-0 after:translate-x-2 after:translate-y-2 lg:after:translate-x-4 lg:after:translate-y-4 after:-z-10">
         <section className="md:flex relative bg-primary border-2 p-8 md:px-16 md:py-20 md:items-center">
           <img
             className="w-full border-2 max-h-52 object-cover object-[center_20%] md:max-w-[300px] md:max-h-max"
